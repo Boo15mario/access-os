21983af4b035a7cd336f3de5eebb828a1d3244f5
maxbaykowski <maxbaykowski@gmail.com>
2025-01-07 19:07:06 -0500

    Fixed an error in build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ if [ "$(id -u)" != "0" ];then
 echo "Error: you must run this script as root" 1>&2
 exit 1
 fi
-if [ "$SUDO_USER ];then
+if [ "$SUDO_USER" ];then
 mkarchiso -v -w /tmp/builds/work -o $SUDO_HOME/builds/out ./iso/access-os/releng/
 else
 mkarchiso -v -w /tmp/builds/work -o $HOME/builds/out ./iso/access-os/releng/
