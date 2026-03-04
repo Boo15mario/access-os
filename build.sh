5f68625d3aeff26af8ed4cb7b565196533fcb207
boo15mario <bala.alek@gmail.com>
2026-03-03 19:12:27 -0600

    chore: use env bash shebang for build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [ "$(id -u)" != "0" ];then
 echo "Error: you must run this script as root" 1>&2
 exit 1
@@ -8,4 +8,3 @@ mkarchiso -v -w /tmp/builds/work -o $SUDO_HOME/builds/out ./iso/access-os/releng
 else
 mkarchiso -v -w /tmp/builds/work -o $HOME/builds/out ./iso/access-os/releng/
 fi
-
